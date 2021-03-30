a7cb651c626b989bf15f20e1ed1d6a139350b9f9
DESKTOP\TheBestWorker <thebestworkerinthecountry@gmail.com>
2021-03-30 18:34:19 +0300

    Created

---
+++ Assets/Scripts/PlanetBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetBehaviour : MonoBehaviour
+{
+    public float speedRotate;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, speedRotate * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayableGOBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableGOBehaviour : MonoBehaviour
+{
+    public float speed;
+    public Vector2 rangePositions;
+
+    private bool isRight = true;
+    private Vector3 targetDirection = Vector3.right;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (isRight && transform.position.x >= rangePositions.y)
+        {
+            targetDirection = Vector3.left;
+            isRight = false;
+        }
+        else if (!isRight && transform.position.x <= rangePositions.x)
+        {
+            targetDirection = Vector3.right;
+            isRight = true;
+        }
+        
+        transform.position += targetDirection * (speed * Time.deltaTime);
+    }
+    
+}
